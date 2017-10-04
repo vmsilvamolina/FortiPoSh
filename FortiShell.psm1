3812c050708dff4d46639953388e4e75b383c53d
Victor Silva <vmsilvamolina@gmail.com>
2017-10-04 10:28:42 -0300

    Add PoSh module

---
+++ FortiShell.psm1
@@ -0,0 +1,29 @@
+﻿function Set-FortigateAccess {
+    [OutputType([String])]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [String]$HostAddress,
+        [Parameter(Mandatory=$false)]
+        [Int]$HostPort = 22,
+        [Parameter(Mandatory=$true)]
+        [String]$Credential,
+        [Parameter(Mandatory=$false)]
+        [String]$Interface,
+        [Parameter(Mandatory=$false)]
+        [String]$AllowAccessOptions
+    )
+ 
+$Command = @"
+config system interface
+edit $Interface
+set allowaccess $AllowAccessOptions
+end
+"@
+ 
+try {
+    ssh $HostAddress -p $HostPort -l $Credential $Command | Out-Null
+} catch {
+    Write-Warning -Message $error[0].exception.message
+}
+}
